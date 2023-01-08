69c16205eaab0fe05c586d5f666a207427bcccd0
raghav8197 <raghav8197@gmail.com>
2023-01-08 17:33:44 +0530

    after changed logout

--- src/Title.js
+++ src/Title.js
@@ -24,11 +24,10 @@ function Title() {
                   <Nav className="me-auto">
                     <Nav.Link onClick={() => navigate('/login')}>Login</Nav.Link>
                     <Nav.Link onClick={() => navigate('/signin')}>SignUp</Nav.Link>
-                    
-                    <Nav.Link onClick={handleLogOut}>Logout</Nav.Link>
                     <Nav.Link onClick={() => navigate('/cart')}>Cart {context.cart.length}</Nav.Link>
                     {role === 'admin' ? <Nav.Link onClick={() => navigate('/adminDashboard')}>DashBoard</Nav.Link> :
                      <Nav.Link onClick={() => navigate('/customerDashboard')}>DashBoard</Nav.Link>}
+                     {role && <Nav.Link onClick={handleLogOut}>Logout</Nav.Link> }
                   </Nav>
               </Container>
             </Navbar>
